7438df9709502bdbcad5b7b92ddb6e1409b762a7
root <root@eksl12.vps.phps.kr>
2020-12-01 14:39:38 +0900

    test

---
+++ node/volumes/index.js
@@ -0,0 +1,13 @@
+var express = require('express');
+
+var app = express();
+var port = 9000;
+
+app.get('/node', function(req, res) {
+	res.send('hello world');
+	console.log('listening!');
+});
+
+app.listen(port, function() {
+	console.log('listening on port ' + port);
+});
