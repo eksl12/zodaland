edb83884e381f582304a3a2e2adf9ca0480850ae
root <root@eksl12.vps.phps.kr>
2020-12-07 11:55:08 +0900

    2018 k q proceeding add

--- php/volumes/test.php
+++ php/volumes/test.php
@@ -1,7 +1,32 @@
 <?php
-	echo 'test';
-	echo 'job';
-	echo 'eksl12';
-	echo 'good';
+	$relation = array(
+array("100", "ryan", "music", "2"),
+array("200", "apeach", "math", "2"),
+array("300", "tube", "computer", "3"),
+array("400", "con", "computer", "4"),
+array("500", "muzi", "music", "3"),
+array("600", "apeach", "music", "2")
+);
 
+
+function solution($relation) {
+
+}
+
+
+function checkUnique($st, $relation) {
+	
+}
+$st = array();
+
+//$aaa = permute($st, 1, 4, array());
+
+
+
+$a = array(1,2,3,4, 5, 6, 7);
+$b = array(array(1,2,3,4), array(1,2,3,4,5,6,7));
+
+if (in_array($a, $b)) {
+	echo 'asdsadsadasdasdas';
+}
 ?>
